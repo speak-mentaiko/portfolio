cead25623f227b90c5f5c31da0b91a60bd8a26b8
speak-mentaiko <1717miyawaki@gmail.com>
2024-06-07 00:31:47 +0900

    add:Tailwindの追加

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,36 @@
+<script>
+  import "../app.css";
+</script>
+
+<div class="font-body">
+  <div class="bg-blue-900/70">
+    <div class="mx-auto max-w-screen-2xl px-6 md:px-6">
+      <header class="flex items-center py-6 md:py-6">
+        <nav class="gap-12 lg:flex">
+          <a
+            href="/"
+            class="text-3xl font-semibold text-white transition duration-100"
+          >
+            Home
+          </a>
+          <a
+            href="/gallery"
+            class="text-3xl font-semibold text-white transition duration-100"
+          >
+            Gallery
+          </a>
+        </nav>
+      </header>
+    </div>
+  </div>
+
+  <div class="bg-sky-100/50">
+    <slot />
+  </div>
+
+  <div class="bg-blue-900/70">
+    <footer class="flex justify-center py-5 md:py-5">
+      <p class="text-white text-sm">Copyright © 2024 speak-mentaiko</p>
+    </footer>
+  </div>
+</div>
---
+++ src/routes/gallery/+page.svelte
@@ -0,0 +1,4 @@
+<div class="gallery">
+  <h2>Gallery</h2>
+  <p>そのうち何か置くかも？</p>
+</div>
