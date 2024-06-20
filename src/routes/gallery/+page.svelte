40ebe3cc8c37c1cf73caa43c1204a62e1f134aac
speak-mentaiko <1717miyawaki@gmail.com>
2024-06-20 15:24:14 +0900

    fix:ギャラリーの内容を追加

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,47 +1,64 @@
 <div class="flex flex-col justify-center items-center text-lg py-4 space-y-4">
-  <h2 class="text-2xl font-semibold">Gallery</h2>
+  <h2 class="text-3xl font-semibold">Gallery</h2>
   <a
     href="https://github.com/speak-mentaiko/BoxRun"
-    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    class="block w-96 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100"
   >
     <img src="/images/boxrun.png" alt="img" />
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
-      BoxRun
-    </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">
-      恐竜ゲームっぽいやつ。RubyとDxRubyで作成
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">BoxRun</h5>
+    <p class="font-normal text-gray-700">
+      恐竜ゲームっぽいやつ<br />
+      RubyとDxRubyで作成
     </p>
   </a>
 
   <a
     href="https://github.com/speak-mentaiko/BoxRun.io"
-    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    class="block w-96 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100"
   >
     <img src="/images/boxrun.io.png" alt="img" />
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
       BoxRun.io
     </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">
-      BoxRunをブラウザで動かしたもの。RubyとDxOpalで作成
+    <p class="font-normal text-gray-700">
+      BoxRunをブラウザで動かしたもの<br />
+      RubyとDxOpalで作成
     </p>
   </a>
 
   <a
     href="https://github.com/speak-mentaiko/portfolio"
-    class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+    class="block w-96 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100"
   >
     <img src="/images/portfolio.png" alt="img" />
-    <h5
-      class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"
-    >
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
       Portfolio
     </h5>
-    <p class="font-normal text-gray-700 dark:text-gray-400">
-      ポートフォリオサイト。SveltekitとTailwindで作成
+    <p class="font-normal text-gray-700">
+      ポートフォリオサイト<br />
+      SveltekitとTailwindで作成
+    </p>
+  </a>
+  <h2 class="text-2xl font-semibold">Others</h2>
+  <a
+    href="https://github.com/mct-joken/WashBoard"
+    class="block w-96 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100"
+  >
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+      WashBoard
+    </h5>
+    <p class="font-normal text-gray-700">
+      第34回高専プロコンで作成<br />
+      自由部門で特別賞/企業賞を受賞
+    </p>
+  </a>
+  <a
+    href="https://github.com/poporonnet/kcmsf"
+    class="block w-96 p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100"
+  >
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">kcmsf</h5>
+    <p class="font-normal text-gray-700">
+      Matz葉がにロボコンの大会運営用に作成
     </p>
   </a>
 </div>
